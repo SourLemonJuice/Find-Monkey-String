84bf1b8490c4107cbb0e303eae7fa4ebb656eb7b
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-20 23:05:47 +0800

    修改众多变量命名且更改了命令行标志
    --printing 被更名为 --print-stream
    新增了 --summary <bool> 以控制是否打印最后的总结
    
    哇，现在的变量命名终于统一了（晕）

--- main.c
+++ main.c
@@ -17,25 +17,27 @@ enum ExitCode {
 int main(int argc, char *argv[])
 {
     // alphabet
-    char Alphabet[] = "abcdefghijklmnopqrstuvwxyz ";
-    uint8_t Alphabet_Length;
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz ";
+    uint8_t alphabet_length;
     // cycle count of main loop
-    uint32_t Cycles = 0;
-    // now random index of array Alphabet
-    uint8_t Now_Random_Char = 0;
-    // which index of Target_String was detected
-    uint8_t Detected_Number = 0;
+    uint32_t cycles = 0;
+    // now random index of array alphabet
+    uint8_t now_random_char = 0;
+    // which index of target_string was detected
+    uint8_t detected_number = 0;
     // and the max cycles, if you don't want your CPU BOOM
     // if set to 0, means no limit
     // or, a timer maybe batter?
-    uint32_t MAX_Cycles = 0;
+    uint32_t max_cycles = 0;
     // whether to print those random char
     bool print_char_bool = true;
+    // whether to print summary
+    bool print_summary = true;
     // target string pointer
-    char *Target_String = "cc";
-    uint8_t Target_Length;
+    char *target_string = "cc";
+    uint8_t target_length;
     // how many arguments did the program get in processing
-    uint8_t Argument_Num = 0;
+    uint8_t argument_num = 0;
 
     srand(time(NULL));
 
@@ -43,15 +45,19 @@ int main(int argc, char *argv[])
     for (int next_opt_num = 1; next_opt_num <= argc - 1; next_opt_num++) {
         if (strcmp(argv[next_opt_num], "--help") == 0 or strcmp(argv[next_opt_num], "-h") == 0) {
             // A help page
-            printf(
-                "Usage: monkey-string [--help | -h] [<Target-String>] [--target-string <string>] [--max-cycles <number "
-                "uint32_t>] [--printing <ture/false>]\n\n");
-            printf("Try to run \"monkey-string hello\" to find word \"hello\". Flag --target-string will do the same "
+            printf("Usage: monkey-string [--help | -h] [--target-string <string>] [--max-cycles <number "
+                   "uint32_t>] [--print-stream <true/false>] [--summary <true/false>] [<Target-String>]\n\n");
+
+            printf("Try to run \"monkey-string hello\" to find word \"hello\". Flag \"--target-string\" will do the same "
                    "thing.\n");
-            printf("If the target string starts with \"--\" or \"-\", only --target-string can work.\n\n");
+            printf("If the target string starts with \"--\" or \"-\", only \"--target-string\" can work.\n\n");
+
             printf("By default, characters will output. This will seriously affect the performance.\n");
-            printf("Use \"--printing false\" to turn is off.\n\n");
+            printf("Use \"--print-stream false\" to turn is off.\n");
+            printf("Also can use the \"--summary false\" flag to get a clean char stream.\n\n");
+
             printf("And... Don't input any character out of the lowercase and space, program won't check it.\n\n");
+
             printf("Published Under MIT License\n");
             printf("By 酸柠檬猹/SourLemonJuice 2024\n");
             return 0;
@@ -59,7 +65,7 @@ int main(int argc, char *argv[])
             // set maximum cycles
             next_opt_num++;
             if (next_opt_num <= argc - 1)
-                MAX_Cycles = atoi(argv[next_opt_num]);
+                max_cycles = atoi(argv[next_opt_num]);
             else
                 goto ERROR_flag_null_value;
             continue;
@@ -67,11 +73,11 @@ int main(int argc, char *argv[])
             // target string
             next_opt_num++;
             if (next_opt_num <= argc - 1)
-                Target_String = argv[next_opt_num];
+                target_string = argv[next_opt_num];
             else
                 goto ERROR_flag_null_value;
             continue;
-        } else if (strcmp(argv[next_opt_num], "--printing") == 0) {
+        } else if (strcmp(argv[next_opt_num], "--print-stream") == 0) {
             // control whether to print random characters
             next_opt_num++;
             if (next_opt_num <= argc - 1) {
@@ -84,60 +90,77 @@ int main(int argc, char *argv[])
             } else
                 goto ERROR_flag_null_value;
             continue;
+        } else if (strcmp(argv[next_opt_num], "--summary") == 0) {
+            // whether to show the summary
+            next_opt_num++;
+            if (next_opt_num <= argc - 1) {
+                if (strcmp(argv[next_opt_num], "true") == 0)
+                    print_summary = true;
+                else if (strcmp(argv[next_opt_num], "false") == 0)
+                    print_summary = false;
+                else
+                    goto ERROR_invalid_value;
+            } else
+                goto ERROR_flag_null_value;
+            continue;
         } else if (strncmp(argv[next_opt_num], "--", 2) == 0 or strncmp(argv[next_opt_num], "-", 1) == 0) {
             printf("Invalid flag: \"%s\"\n", argv[next_opt_num]);
             return kExitCodeStdError;
         }
 
-        if (Argument_Num >= 1) {
-            Argument_Num++;
+        if (argument_num >= 1) {
+            argument_num++;
             printf("Too many arguments: \"%s\"\n", argv[next_opt_num]);
             return kExitCodeStdError;
-        } else if (Argument_Num == 0) {
-            Argument_Num++;
+        } else if (argument_num == 0) {
+            argument_num++;
             // the first argument same like "--target-string"
-            Target_String = argv[next_opt_num];
+            target_string = argv[next_opt_num];
             continue;
         }
     }
 
     // when all setted down
     // get some length
-    Alphabet_Length = strlen(Alphabet);
-    Target_Length = strlen(Target_String);
+    alphabet_length = strlen(alphabet);
+    target_length = strlen(target_string);
     /* Main loop block */
-    while (MAX_Cycles == 0 or Cycles < MAX_Cycles) {
+    while (max_cycles == 0 or cycles < max_cycles) {
         /* get new random char */
-        Now_Random_Char = rand() % Alphabet_Length;
+        now_random_char = rand() % alphabet_length;
         /* print? */
         if (print_char_bool == true)
-            putchar(Alphabet[Now_Random_Char]);
+            putchar(alphabet[now_random_char]);
 
         /* detection */
-        if (Alphabet[Now_Random_Char] == Target_String[Detected_Number])
-            Detected_Number++;
+        if (alphabet[now_random_char] == target_string[detected_number])
+            detected_number++;
         else
-            Detected_Number = 0;
+            detected_number = 0;
 
         /* increase cycle counter */
-        Cycles++;
+        cycles++;
 
         /* if it's the end, break loop. */
-        if (Detected_Number == Target_Length)
+        if (detected_number == target_length)
             break;
     }
     // Break last putchat(), and make a dividing line
     if (print_char_bool == true)
         printf("\n");
-    printf("======== [Summary] ========\n");
-    printf("The target string is \"%s\"\n", Target_String);
-    if (Cycles == MAX_Cycles) {
-        printf("The number of cycles may have reached the MAX limit.\n");
-        printf("Cycles: %d, Max Limit: %d\n", Cycles, MAX_Cycles);
-    } else {
-        printf("Task Success\n");
-        printf("It took %d cycles\n", Cycles);
+
+    if (print_summary == true) {
+        printf("======== [Summary] ========\n");
+        printf("The target string is \"%s\"\n", target_string);
+        if (cycles == max_cycles) {
+            printf("The number of cycles may have reached the MAX limit.\n");
+            printf("Cycles: %d, Max Limit: %d\n", cycles, max_cycles);
+        } else {
+            printf("Task Success\n");
+            printf("It took %d cycles\n", cycles);
+        }
     }
+
     return kExitCodeSuccess;
 
 /* meow~ */
